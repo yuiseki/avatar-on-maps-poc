0be0acd0ee41539e7b3a7595402f052b92139188
yuiseki <yuiseki@gmail.com>
2023-02-25 21:48:51 +0900

    台東区役所がダブってるので消す

--- src/points.tsx
+++ src/points.tsx
@@ -1,11 +1,5 @@
 export const points = [
   {
-    latitude: 35.712583,
-    longitude: 139.780056,
-    title: "台東区役所",
-    emoji_reactions: {},
-  },
-  {
     latitude: 35.7100576,
     longitude: 139.7728115,
     title: "スシロー 上野店",
